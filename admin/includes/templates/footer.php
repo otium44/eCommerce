0358113b9c0a8078739cecfa379ae5dfdbe9f73c
otium44 <yasseralibgy@gmail.com>
2022-09-08 16:34:03 +0300

    some updates

--- admin/includes/templates/footer.php
+++ admin/includes/templates/footer.php
@@ -1,5 +1,5 @@
-<div class="footer">
-			This is Footer
-		</div>
-	</body>
+<script src="layout/js/popper.min.js"></script>
+  <script type="text/javascript" src="layout/js/jquery-3.6.1.min.js"></script>
+  <script type="text/javascript" src="layout/js/bootstrap.js"></script>
+</body>
 </html>
