74694df4fe342b325be72cc7d1a46110513e6aac
z3sb <oslvuao@gmail.com>
2022-09-08 16:23:43 +0300

    add Bootstrap and jQuery files

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,20 @@
 <?php include "includes/templates/header.php"; ?>
-
-Welcome to index
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="layout/css/bootstrap.css">
+  <title>eCommerce</title>
+</head>
+<body>
+  <div class="alert alert-success">
+    Hello
+  </div>
+  <script src="layout/js/popper.min.js"></script>
+  <script type="text/javascript" src="layout/js/jquery-3.6.1.min.js"></script>
+  <script type="text/javascript" src="layout/js/bootstrap.js"></script>
+</body>
+</html>
 <?php include "includes/templates/footer.php"; ?>
