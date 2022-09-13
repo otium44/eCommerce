ed8cf058060201982a69dd308ae77dc02782e7a3
z3sb <oslvuao@gmail.com>
2022-09-13 17:22:10 +0300

    Design Dashboard Page

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -3,17 +3,73 @@
 
   if (isset($_SESSION['Username'])) {
 
-        $pageTitle = 'Dashboard';
+    $pageTitle = 'Dashboard';
 
-        include 'init.php';
+    include 'init.php';
 
-        echo 'Welcome';
+    /* Start Dashboard Page */ ?>
 
-        include $tpl . 'footer.php';
+    <div class="home-stats">
+      <div class="container text-center">
+        <h1>Dashboard</h1>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="stat">
+              Total Members
+              <strong>200</strong>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="stat">
+              Pending Members
+              <strong>100</strong>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="stat">
+              Total Items
+              <strong>20</strong>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="stat">
+              Total Comments
+              <strong>2030</strong>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="latest">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-header"><i class="fa fa-users"></i> Latest Registerd Users</div>
+              <div class="card-body">
+                Test
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-header"><i class="fa fa-tag"></i> Latest Items</div>
+              <div class="card-body">
+                Test
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php
+    /* End Dashboard Page */
+
+    include $tpl . 'footer.php';
 
   } else {
 
-        header('Location: index.php');
-        exit();
+    header('Location: index.php');
+    exit();
 
   }
